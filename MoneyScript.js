10040d6bc764160eec6d6a45a61dce609f0793cf
elvinibbotson <elvinibbotson@outlook.com>
2025-08-04 14:14:35 +0100

    Add files via upload

--- MoneyScript.js
+++ MoneyScript.js
@@ -717,7 +717,7 @@ if(navigator.serviceWorker.controller) {
 	console.log('Active service worker found, no need to register')
 }
 else { // Register the ServiceWorker
-	navigator.serviceWorker.register('moneySW.js', {scope: '/scrooge/'}).then(function(reg) {
+	navigator.serviceWorker.register('sw.js', {scope: '/Money/'}).then(function(reg) {
 	    console.log('Service worker has been registered for scope:'+ reg.scope);
 	});
 }
